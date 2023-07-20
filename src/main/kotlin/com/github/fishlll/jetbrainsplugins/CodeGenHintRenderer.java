bcf2faa76442fff59b62a70cb5e4b402bf709f25
liangliang.liu <liangliang.liu@qunar.com>
2023-07-20 15:52:27 +0800

    同步starcoder-intellij.git 中的java代码

---
+++ src/main/kotlin/com/github/fishlll/jetbrainsplugins/CodeGenHintRenderer.java
@@ -0,0 +1,22 @@
+package com.github.fishlll.jetbrainsplugins;
+
+import com.intellij.codeInsight.daemon.impl.HintRenderer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import org.jetbrains.annotations.Nullable;
+
+public class CodeGenHintRenderer extends HintRenderer {
+    public CodeGenHintRenderer(@Nullable String text) {
+        super(text);
+    }
+
+    @Override
+    protected TextAttributes getTextAttributes(Editor editor) {
+        // TODO custom color schemes?
+        TextAttributes newAttributes = new TextAttributes();
+        newAttributes.copyFrom(editor.getColorsScheme().getAttributes(DefaultLanguageHighlighterColors.LINE_COMMENT));
+        return newAttributes;
+    }
+
+}
